11bb389f1b04db9dce1f9d70170f42c70ae2836f
Stefan Brandl <steve260897@gmail.com>
2022-12-01 14:34:55 +0100

    fancy insertion sort

--- 01/01.c
+++ 01/01.c
@@ -2,12 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LINE_LEN 10
+#define LINE_LEN 100
+#define N_ELVES 3
+#define LAST_INDEX N_ELVES - 1
 
 struct solution {
     int p1;
     int p2;
 };
+
+void insertion_sort(int* data, int value) {
+    if (value <= data[LAST_INDEX]) return;
+
+    data[LAST_INDEX] = value;
+
+    for (int i = LAST_INDEX - 1; i >= 0; i--) {
+        if (data[i] < data[i + 1]) {
+            value = data[i];
+            data[i] = data[i + 1];
+            data[i + 1] = value;
+        }
+    }
+}
+
+int* init_array(size_t len) {
+    int* data = malloc(sizeof(int) * N_ELVES);
+    if (data == NULL) return NULL;
+
+    for (int i = 0; i < N_ELVES; i++) {
+        data[i] = 0;
+    }
+    return data;
+}
 
 struct solution* solve(const char* file_name) {
     FILE* fp = fopen(file_name, "r");
@@ -16,25 +42,24 @@ struct solution* solve(const char* file_name) {
         return NULL;
     }
 
-    int elf_1 = -1;
-    int elf_2 = -1;
-    int elf_3 = -1;
-    int curr_elf = 0;
+    int* data = init_array(N_ELVES);
+    if (data == NULL) {
+        fclose(fp);
+        return NULL;
+    }
 
-    char line[LINE_LEN];
-    while (fgets(line, sizeof(line), fp)) {
+    char* line = malloc(sizeof(char) * LINE_LEN);
+    if (line == NULL) {
+        fclose(fp);
+        free(data);
+        return NULL;
+    }
+
+    int curr_elf = 0;
+    while (fgets(line, sizeof(char) * LINE_LEN, fp)) {
         // empty line
         if (line[0] == '\n') {
-            if (curr_elf > elf_1) {
-                elf_3 = elf_2;
-                elf_2 = elf_1;
-                elf_1 = curr_elf;
-            } else if (curr_elf > elf_2) {
-                elf_3 = elf_2;
-                elf_2 = curr_elf;
-            } else if (curr_elf > elf_3) {
-                elf_3 = curr_elf;
-            }
+            insertion_sort(data, curr_elf);
             curr_elf = 0;
         } else {
             int val = atoi(line);
@@ -46,10 +71,18 @@ struct solution* solve(const char* file_name) {
         }
     }
     fclose(fp);
+    free(line);
 
     struct solution* sol = malloc(sizeof(struct solution));
-    sol->p1 = elf_1;
-    sol->p2 = elf_1 + elf_2 + elf_3;
+    if (sol == NULL) return NULL;
+
+    sol->p1 = data[0];
+    sol->p2 = sol->p1;
+    for (int i = 1; i < N_ELVES; i++) {
+        sol->p2 += data[i];
+    }
+    free(data);
+
     return sol;
 }
 
