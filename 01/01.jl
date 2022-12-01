9de869b4e75ad7492f87d139052e3533f02c1fc3
Stefan Brandl <steve260897@gmail.com>
2022-12-01 16:28:28 +0100

    day 01 julia

---
+++ 01/01.jl
@@ -0,0 +1,10 @@
+data = open("01/input.txt") do in_file
+    content = read(in_file, String)
+    sort(
+        collect(sum(parse.(Int, eachsplit(elf, "\n"))) for elf in eachsplit(content, "\n\n")),
+        rev=true
+    )[1:3]
+end
+
+println("part1: ", data[1])
+println("part2: ", sum(data))
