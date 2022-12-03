3e62602e1dc50dee3e2f834c0bc185aa5634850c
Stefan Brandl <steve260897@gmail.com>
2022-12-03 15:41:04 +0100

    day 02 in julia

---
+++ 03/03.jl
@@ -0,0 +1,33 @@
+timestamp_nano() = Int(round(time() * 1e9))
+
+score(char)::Int = ifelse(char <= 'Z',
+    char - 'A' + 27,
+    char - 'a' + 1
+)
+
+part1(data)::Int = sum(map(
+    score,
+    # should only be one intersection anyways
+    first(
+        line[1:length(line)÷2] ∩ line[length(line)÷2+1:end]
+    ) for line in data
+))
+
+part2(data)::Int = sum(map(
+    score,
+    first(
+        data[i] ∩ data[i+1] ∩ data[i+2]
+    ) for i in 1:3:length(data)
+))
+
+start = timestamp_nano()
+
+data = open("03/input.txt") do in_file
+    split(read(in_file, String), "\n")
+end
+
+println("part1: ", part1(data))
+println("part2: ", part2(data))
+
+end_ = timestamp_nano()
+println("time: ", round((end_ - start) / 1000, digits=1), "µs")
