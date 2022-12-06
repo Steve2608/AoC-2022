8e2ea654f9fd42cd372ade61ed6c00dad9e70534
Stefan Brandl <steve260897@gmail.com>
2022-12-06 15:09:23 +0100

    Added comments for reproducability

--- 06/06_c_stuff.py
+++ 06/06_c_stuff.py
@@ -9,14 +9,22 @@ if __name__ == '__main__':
         c_data = create_string_buffer(in_file.read())
         c_data_len = c_size_t(len(c_data))
 
+    # create a shared object first
     # gcc -Wall -std=c17 -pedantic -O2 -shared 06/06.c -o 06/lib06.so
+    # load shared object
     lib06 = CDLL("06/lib06.so")
 
-    p1 = lib06.solve(c_data, c_data_len, c_size_t(4), c_int(0))
+    # get the function from the shared object
+    solve = lib06.solve
+
+    # explicitly (!) set the return-type
+    solve.restype = c_int
+
+    p1 = solve(c_data, c_data_len, c_size_t(4), c_int(0))
     print(f'part1: {p1}')
 
     c_p1 = c_int(p1)
-    p2 = lib06.solve(c_data, c_data_len, c_size_t(14), c_p1)
+    p2 = solve(c_data, c_data_len, c_size_t(14), c_p1)
     print(f'part2: {p2}')
 
     end = timestamp_nano()
