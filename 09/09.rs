998f7943b5bbcd68098d54ff6c2a7e611691b83c
Stefan Brandl <steve260897@gmail.com>
2022-12-09 13:12:30 +0100

    processed part1/2 at the same time

--- 09/09.rs
+++ 09/09.rs
@@ -18,48 +18,20 @@ fn main() {
         })
         .collect();
 
-    println!("part1: {}", part1(&instr));
-    println!("part2: {}", part2(&instr, 9));
+    let (p1, p2) = part12(&instr, 9);
+    println!("part1: {}", p1);
+    println!("part2: {}", p2);
 
     println!("time: {:?}", start.elapsed());
 }
 
-fn part1(instructions: &Vec<(&str, usize)>) -> usize {
-    let mut visited: HashSet<(i16, i16)> = HashSet::new();
-
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-    visited.insert(tail);
-
-    for &(direction, amount) in instructions.iter() {
-        for _ in 0..amount {
-            match direction {
-                "U" => head.1 += 1,
-                "D" => head.1 -= 1,
-                "L" => head.0 -= 1,
-                "R" => head.0 += 1,
-                _ => unreachable!()
-            }
-
-            // drag tail behind
-            let diff_x = head.0 - tail.0;
-            let diff_y = head.1 - tail.1;
-        
-            if diff_x.abs() > 1 || diff_y.abs() > 1 {
-                tail.0 += diff_x.signum();
-                tail.1 += diff_y.signum();
-            }
-            visited.insert(tail);
-        }
-    }
-    return visited.len();
-}
-
-fn part2(instructions: &Vec<(&str, usize)>, n_knots: usize) -> usize {
-    let mut visited: HashSet<(i16, i16)> = HashSet::new();
+fn part12(instructions: &Vec<(&str, usize)>, n_knots: usize) -> (usize, usize) {
+    let mut visited1: HashSet<(i16, i16)> = HashSet::new();
+    let mut visited2: HashSet<(i16, i16)> = HashSet::new();
 
     let mut knots: Vec<(i16, i16)> = vec![(0, 0); n_knots + 1];
-    visited.insert(knots[n_knots]);
+    visited1.insert(knots[n_knots]);
+    visited2.insert(knots[n_knots]);
 
     for &(direction, amount) in instructions.iter() {
         for _ in 0..amount {
@@ -75,14 +47,15 @@ fn part2(instructions: &Vec<(&str, usize)>, n_knots: usize) -> usize {
             for i in 1..=n_knots {
                 let diff_x = knots[i - 1].0 - knots[i].0;
                 let diff_y = knots[i - 1].1 - knots[i].1;
-            
+
                 if diff_x.abs() > 1 || diff_y.abs() > 1 {
                     knots[i].0 += diff_x.signum();
                     knots[i].1 += diff_y.signum();
                 }
             }
-            visited.insert(knots[n_knots]);
+            visited1.insert(knots[1]);
+            visited2.insert(knots[n_knots]);
         }
     }
-    return visited.len();
+    return (visited1.len(), visited2.len());
 }
