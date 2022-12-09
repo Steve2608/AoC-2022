b9bd5138e514501f3688b21703cf187334148409
Stefan Brandl <steve260897@gmail.com>
2022-12-09 11:37:53 +0100

    simplified messy code

--- 08/08.rs
+++ 08/08.rs
@@ -1,3 +1,4 @@
+use std::cmp::max;
 use std::fs;
 use std::time::Instant;
 
@@ -5,12 +6,11 @@ fn main() {
     let start = Instant::now();
 
     let data: String = fs::read_to_string("08/input.txt").expect("File not found");
-    let grid: Vec<Vec<u8>> = data.split("\n")
-        .map(|line| {
-            line.as_bytes().iter().map(|num| num - b'0').collect()
-        })
+    let grid: Vec<Vec<u8>> = data
+        .split("\n")
+        .map(|line| line.as_bytes().iter().map(|num| num - b'0').collect())
         .collect();
-    
+
     println!("part1: {}", part1(&grid));
     println!("part2: {}", part2(&grid));
 
@@ -19,67 +19,68 @@ fn main() {
 
 fn part1(grid: &Vec<Vec<u8>>) -> usize {
     // no double counting on corners
-    let mut i = grid.len()*2 + grid[0].len()*2 - 4;
+    let mut n_visible = grid.len() * 2 + grid[0].len() * 2 - 4;
+
     for x in 1..grid.len() - 1 {
         let row = &grid[x];
 
-        for y in 1..&row.len() - 1 {
-            let tree = &row[y];
+        for y in 1..grid[x].len() - 1 {
+            let tree = grid[x][y];
 
-            if row[y+1..row.len()].iter().all(|t| tree > t) || row[0..y].iter().rev().all(|t| tree > t) {
-                i += 1;
-            } else {
-                // construction of col-vec is expensive, so we check for rows only first
-                let col = grid.iter()
-                    .map(|s| &s[y])
-                    .collect::<Vec<_>>();
-
-                if col[x+1..col.len()].iter().all(|t| tree > t) || col[0..x].iter().rev().all(|t| tree > t) {
-                    i += 1;
-                }
+            if row[y + 1..grid[x].len()].iter().all(|t| tree > *t)
+                || row[0..y].iter().rev().all(|t| tree > *t)
+                || grid[x + 1..grid.len()].iter().all(|t| tree > t[y])
+                || grid[0..x].iter().rev().all(|t| tree > t[y])
+            {
+                n_visible += 1;
             }
         }
     }
-    return i
+    return n_visible;
 }
 
 fn part2(grid: &Vec<Vec<u8>>) -> usize {
     let mut scenic_score = 0;
-    for x in 1..grid.len() - 1 {
+    for x in 0..grid.len() {
         let row = &grid[x];
 
-        for y in 1..&row.len() - 1 {
-            let tree = &row[y];
+        for y in 0..row.len() {
+            let tree = row[y];
 
             let mut score_up = 1;
             for i in (1..x).rev() {
-                if tree <= &grid[i][y] { break; }
+                if tree <= grid[i][y] {
+                    break;
+                }
                 score_up += 1;
             }
 
             let mut score_left = 1;
             for i in (1..y).rev() {
-                if tree <= &grid[x][i] { break; }
+                if tree <= grid[x][i] {
+                    break;
+                }
                 score_left += 1;
             }
 
             let mut score_down = 1;
-            for i in x+1..grid.len() - 1 {
-                if tree <= &grid[i][y] { break; } 
+            for i in x + 1..grid.len() - 1 {
+                if tree <= grid[i][y] {
+                    break;
+                }
                 score_down += 1;
             }
 
             let mut score_right = 1;
-            for i in y+1..grid[x].len() - 1 {
-                if tree <= &grid[x][i] { break; }
+            for i in y + 1..grid[x].len() - 1 {
+                if tree <= grid[x][i] {
+                    break;
+                }
                 score_right += 1;
             }
 
-            let s = score_up * score_left * score_down * score_right;
-            if s > scenic_score {
-                scenic_score = s;
-            }
+            scenic_score = max(scenic_score, score_up * score_left * score_down * score_right);
         }
     }
-    return scenic_score
+    return scenic_score;
 }
